dfb1e283b125701bfc3fe446d6340d8231346ba1
nivya <nivya.mathew@vonnue.com>
2022-11-17 15:50:22 +0530

    feat:added etsy

---
+++ banner/img/Etsy India - Shop for handmade, vintage, custom, and unique gifts for everyone_files/config.js
@@ -0,0 +1,6 @@
+
+window[window["TiktokAnalyticsObject"]].instance("CD255AJC77U2F908QPL0").setAdvancedMatchingAvailableProperties({"email":true,"phone_number":true,"auto_email":true,"auto_phone_number":false});
+window[window["TiktokAnalyticsObject"]].instance("CD255AJC77U2F908QPL0").setPixelInfo && window[window["TiktokAnalyticsObject"]].instance("CD255AJC77U2F908QPL0").setPixelInfo({status: 0, name: "Etsy Paid", advertiserID: "6903627353621004289", setupMode: 0, partner: "", is_onsite: false });
+window[window["TiktokAnalyticsObject"]].setPCMDomain && window[window["TiktokAnalyticsObject"]].setPCMDomain("etsy.com");
+window[window["TiktokAnalyticsObject"]].setPCMConfig && window[window["TiktokAnalyticsObject"]].setPCMConfig(null);
+window[window["TiktokAnalyticsObject"]].enableFirstPartyCookie && window[window["TiktokAnalyticsObject"]].enableFirstPartyCookie(true);
