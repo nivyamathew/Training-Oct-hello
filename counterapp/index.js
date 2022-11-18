75ee6b8f0b062782d2acda60836508a623560f13
nivya <nivya.mathew@vonnue.com>
2022-11-18 19:48:55 +0530

    feat: added instacart to motherpage

---
+++ counterapp/index.js
@@ -0,0 +1,29 @@
+//document.getElementById("count-el").innerText = 6
+
+//let count = 0
+
+//console.log(count) 
+let saveEl = document.getElementById("save-el")
+ let countEl = document.getElementById("count-el")
+ let count = 0
+console.log(saveEl)
+
+function increment() {
+   count = count + 1
+    countEl.textContent = count
+}
+function save(){
+    let countStr = count + " - "
+
+    saveEl.textContettStr
+    countEl.textContent = 0
+    count = 0
+ }
+//let welcomeEl = document.getElementById("welcome-el")
+//let user = "ABCDE"
+//let greeting = "Welcome back "
+//welcomeEl.innerText = greeting + user
+//welcomeEl.innerText = welcomeEl.innerText + "🤗"
+//welcomeEl.innerText += "🤗"
+
+ 
