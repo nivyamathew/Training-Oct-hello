b6be231427eaa89de072ef0b1507f0fe527450e7
nivya <nivya.mathew@vonnue.com>
2022-12-10 04:19:18 +0530

    feat:tutorial3

--- Js-tutorial-3/index.js
+++ Js-tutorial-3/index.js
@@ -5,3 +5,20 @@
 // let isApproved = false;
 // let firstName = undefined;
 // let selectColor = null; 
+ourReusableFunction()
+function reusableFunction(){
+    console.log("Hi world")
+    reusableFunction()
+}
+console.log(reusableFunction())
+function ourReusableFunction(){
+    console.log("Heyy, world")
+}
+ourReusableFunction()
+ourReusableFunction()
+ourReusableFunction()
+ourReusableFunction()
+console.log(ourReusableFunction)
+function ourFunctionWithArgs(a,b){
+    console.log(a-b);
+ }
