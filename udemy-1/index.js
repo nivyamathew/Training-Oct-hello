3a9537a2e166e0b85ad60036d2c5371c640ecb58
nivya <nivya.mathew@vonnue.com>
2022-12-13 17:53:54 +0530

    feat:section 2 completed

---
+++ udemy-1/index.js
@@ -0,0 +1,188 @@
+// let js = "amazing";
+// if (js === "amazing") alert("Javascript is Fun!");
+// 40 + 8 + 23 - 10;
+// console.log(40 + 8 + 23 -10);
+// let year;
+// console.log(year);
+// console.log(typeof year)
+// year = 1991;
+// console.log(typeof year);
+// console.log(typeof null);
+// let age = 30;
+// age = 31;
+// const birthYear =1991;
+// // birthYear = 1999;
+// console.log(age);
+// console.log(birthYear);
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
+// console.log(ageJonas, ageSarah);
+// console.log(ageJonas * 2, ageSarah / 10,2**3)
+// const firstName = 'Jonas';
+// const lastName = 'Sarah';
+// console.log(lastName + ' '+ firstName);
+// let x = 10 + 5;    
+// x += 10;
+// x *= 10;
+// console.log(x);
+// console.log(ageJonas > ageSarah);
+// console.log(ageSarah >= 18); 
+// const isFullAge = ageSarah >= 18;
+// console.log(now - 1991 > now - 2018)
+// let massOfJohn = 78;
+// let massOfMark = 92;
+// let heightOfJohn = 1.69;
+// let heightOfMark = 1.95;
+// // let massOfJohn = 95;
+// // let massOfMark = 85;
+// // let heightOfJohn = 1.88;
+// // let heightOfMark = 1.76;
+// let bmiOfJohn = massOfJohn / (heightOfJohn ** 2);
+// console.log(bmiOfJohn);
+// let bmiOfMark = massOfMark / (heightOfMark ** 2);
+// console.log(bmiOfMark);
+// const markHigher = bmiOfJohn > bmiOfMark
+// console.log(markHigher)
+// 13/12
+// const firstName = 'Jonas';
+// const job = 'teacher';
+// const birthYear = 1991;
+// const year = 2037;
+// const Jonas = "I'm " + firstName + ', a ' + (year - birthYear) + ' year old ' + job +'!';
+// console.log(Jonas)
+// const jonasNew = `I'm  ${firstName}, a ${year - birthYear} year old ${job}!`
+// console.log(jonasNew);
+// console.log(`Just a regular string...`);
+// console.log(`string with \n multiple \n lines`);
+// console.log(`string
+// with multiple
+// // lines`)
+// const age = 54;
+// // const isOldEnough = age >= 18;
+// if(age >= 18){
+//     console.log('Sarah can start driving license 😱');
+// } else {
+//   const yearLeft = 18-age;
+//   console.log(`Sarah is too young. Wait another ${yearLeft} years :) `);  
+// }
+// const birthYear = 1998;
+//  let century; 
+// if(birthYear <= 2000){
+//     let century = 20;
+// } else {
+//     let century =21;
+// }
+// console.log(century);
+// if (bmiOfMark > bmiOfJohn) {
+//     console.log(`Mark's BMI (${bmiOfMark}) is higher than John's (${bmiOfJohn}) !`)
+// } else {
+//     console.log(`John's BMI (${bmiOfJohn})  is higher than Mark's (${bmiOfMark}) !`)
+
+// }
+// const inputYear = '1991';
+// console.log(Number(inputYear));
+// console.log(Number(inputYear) + 18);
+// console.log(Number('Jonas'));
+// console.log(typeof NaN);
+// console.log(String(23), 23);
+// console.log('I am ' + 23 + ' years old')
+// console.log('23' + '10' +3);
+// let n = '1'+1;
+// n = n-1;
+// console.log(n);
+// console.log(Boolean(0));
+// console.log(Boolean(undefined));
+// console.log(Boolean('Jonas'));
+// console.log(Boolean({}));
+//  const money = 140;
+//  if(money) {
+//     console.log("Don't send it at all;)");
+//  } else {
+//     console.log('You should get a job!');
+//  }
+// let height = 0;
+// if(height){
+//     console.log('YAY! Height is defined');
+// } else {
+//     console.log('Height is UNDEFINED')
+// }
+// const age = 19 ;
+// if(age == 18)
+// console.log('You just became an adult');
+
+// prompt("what's your favourite number");
+// const favourite = Number(prompt("What's your favourite number?"))
+// console.log(favourite);
+// console.log(typeof favourite);
+// if (favourite === 23) {
+//     console.log('Cool! 23  is an amazing number!');
+// } else if (favourite === 7) {
+//     console.log('7 is also a cool number');
+// } else if (favourite === 9) {
+//     console.log('9 is also a cool number');
+// } else {
+//     console.log('Number is not 23 or 7 or 9');
+// }
+// if (favourite != 23) console.log('why not 23?');
+// const hasDriversLicense = true;
+// const hasGoodVision = true;
+// console.log(hasDriversLicense && hasGoodVision);
+// console.log(hasDriversLicense || hasGoodVision);
+// console.log(hasDriversLicense);
+// const shouldDrive = hasDriversLicense && hasGoodVision;
+// // if(shouldDrive){
+// //     console.log('Sarah is able to drive!');
+// // } else {
+// //     console.log("Someone else should drive...");
+// // }
+// const isTired = true;
+// console.log(hasDriversLicense && hasGoodVision && isTired);
+//     // const hasDriversLicense: true
+//     if (hasDriversLicense && hasGoodVision && !isTired){
+//         console.log("Sarah is able to drive");
+//     } else {
+//         console.log("Someone else should drive...");
+//     }
+
+// const scoreDolphins = (96+108+89)/3;
+// const scoreKolas = (88+91+110)/3;
+// console.log(scoreDolphins, scoreKolas);
+// if(scoreDolphins > scoreKolas){
+//     console.log('Dolphins win the trophy 🏆');
+// } else if (scoreKolas > scoreDolphins){
+//     console.log('Kolas win the trophy 🏆');
+// }else if(scoreDolphins === scoreKolas){
+//     console.log('Both win the trophy 🏆');
+// }
+
+// 3 + 4
+// 1991
+// true && false && !false
+// if (23 > 10) {
+//     const str = '23 is bigger'
+// }
+// const me = 'Jonas'
+// console.log(`I'm ${2037 - 1991} years old ${me}`)
+
+// const age = 25;
+// age >= 18 ? console.log('I like to drink wine 🍷'):
+// console.log('I like to drink water 💧');
+
+// const drink = age >= 18 ? 'wine 🍷': 'water 💧';
+// console.log(drink);
+// let drink2;
+// if(age >= 18){
+//     drink2 = 'wine 🍷';
+// } else {
+//     drink2 = 'water 💧';
+// }
+// console.log(drink2);
+
+// // const drink = age >= 18 ? 'wine 🍷': 'water 💧';
+// console.log(`I like to drink ${age >= 18? 'wine 🍷': 'water 💧' }`);
+ 
+const bill = 400
+const tip = bill <= 300 && bill >= 50 ? bill * 0.15:
+bill * 0.2;
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
