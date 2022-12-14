4c97856f83d9b9ed9728f3b0061d95fa096d555a
nivya <nivya.mathew@vonnue.com>
2022-12-14 19:59:44 +0530

    feat: section 3 and 4 completed

--- Udemy/section-3/index.js
+++ Udemy/section-3/index.js
@@ -153,7 +153,7 @@
 //     'Jonas',
 //     'Schmedtmann'
 //     2037 - 1991
-//     'teacher'
+//     'teacher'-----
 //     ['Micheal', 'Peter','Steven']
 // ];
 
@@ -259,40 +259,93 @@
 //     console.log(`Lifting weights repetition ${rep} 🏋️‍♀️`);
 // }
 
-const jonas = [
-    'jonas',
-    'Schmedtmann',
-    2037 - 1991,
-    'teacher',
-    ['Micheal', 'Peter', 'Steven']
-];
-const types = []
-for(let i = 0;i< jonas.length ;i++){
-    console.log(jonas[i], typeof jonas[i]);
-    types[i] = typeof jonas[i];
-}
-console.log(types);
-const years = [1991, 2007, 1969, 2020];
-const ages = [];
-for(let i = 0; i < years.length; i++){
-    ages.push(2037 -years[i]);
-}
-console.log(ages);
+// const jonas = [
+//     'jonas',
+//     'Schmedtmann',
+//     2037 - 1991,
+//     'teacher',
+//     ['Micheal', 'Peter', 'Steven']
+// ];
+// const types = []
+// for(let i = 0;i< jonas.length ;i++){
+//     console.log(jonas[i], typeof jonas[i]);
+//     types[i] = typeof jonas[i];
+// }
+// console.log(types);
+// const years = [1991, 2007, 1969, 2020];
+// const ages = [];
+// for(let i = 0; i < years.length; i++){
+//     ages.push(2037 -years[i]);
+// }
+// console.log(ages);
 
-console.log('---ONLY STRINGS ---')
-for (let i = 0; i < jonas.length; i++){
-    if(typeof jonas[i] !== 'string') break;
-    console.log(jonas[i], typeof jonas[i]);
-}
+// console.log('---ONLY STRINGS ---')
+// for (let i = 0; i < jonas.length; i++){
+//     if(typeof jonas[i] !== 'string') break;
+//     console.log(jonas[i], typeof jonas[i]);
+// }
 
+// 15/12/2022//
 
+// const jonas = [
+//     'Jonas',
+//     'Schmedtmann',
+//     2037 - 1991,
+//     'teacher',
+//     ['Micheal', 'Peter', 'steven']
+// ];
 
+// for (let i = jonas.length - 1; i >= 0; i--) {
+//     console.log(i, jonas[i]);
+// }
+// for (let exercise = 1; exercise <= 3; exercise++) {
+//     console.log(`-------Starting exercise----------- ${exercise}`);
 
+// for (let rep = 1; rep < 6; rep++){
+//     console.log(`Llifting weight repetition ${rep}`);
+// }
+// }
+// for (let rep = 1; rep <= 10; rep++){
+//     console.log(`Lifting weights repetition ${rep} 🏋️‍♀️`)
+// }
+// let rep = 1;
+// while(rep <= 10){
+//     console.log(` While: Lifting weights repetition ${rep} 🏋️‍♀️`);
+//     rep++;
+// }
+// let dice = Math.trunc(Math.random() * 6) + 1;
+// console.log(dice);
 
+// while(dice !== 6){
+//     console.log(`You rolled a ${dice}`);
+//     dice = Math.trunc(Math.random()*6) + 1;
+//     if (dice === 6) console.log('Loop is about to end...');
+// }
 
-
-
-
+// const calcTip = function (bill){
+//     return bill >= 50 && bill <= 300 ? bill * 0.15: bill * 0.2;
+// }
+// const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+// const tips = [];
+// const totals = [];
+// for(let i = 0; i < bills.length; i++){
+//     const tip = calcTip(bills[i]);
+//     tips.push(tip);
+//     totals.push(tip + bills [i]);
+// }
+// console.log(bills, tips, totals);
+// const calcAverage = function(arr){
+//     let sum = 0;
+//     for(let i = 0; i < arr.length; i++){
+//         // sum = sum + arr[i];
+//         sum += arr[i];
+//     }
+//     console.log(sum);
+//     return sum / arr.length;
+// }
+// console.log(calcAverage ([2, 3, 7]));
+// console.log(calcAverage(totals));
+// console.log(calcAverage(tips));
 
 
 
