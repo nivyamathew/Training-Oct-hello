6056bd999cc14ed83e818c06cb6af43898087142
nivya <nivya.mathew@vonnue.com>
2022-12-16 12:04:22 +0530

    feat: section 3 and 5

--- Udemy/section-5/index.js
+++ Udemy/section-5/index.js
@@ -50,7 +50,7 @@ console.log(`...${data1[0]}°c... ${data1[1]}°c...${data1[2]}°c...`);
 const printForecast = function(arr){
   let str = '';
   for(let i = 0;i < arr.length; i++){
-    str = str + `${arr[i]}°c in ${i + 1} days...`; 
+    str = str + `${arr[i]}°c in ${i + 1} dayscode cd ...`; 
   }
   console.log(str);
 };
