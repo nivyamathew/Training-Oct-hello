4f18d90c0711ce906199eec39cb2d198a0672b1d
nivya <nivya.mathew@vonnue.com>
2022-12-18 15:28:52 +0530

    feat: section-8

--- Udemy/section-8/script.js
+++ Udemy/section-8/script.js
@@ -10,32 +10,42 @@
 //     const job = 'teacher';
 //     console.log(`${myName} is a ${age}-old ${job}`)
 // }
-function calcAge(birthYear){
-    const age = 2037 - birthYear;
-    // console.log(firstName);
-    function printAge(){
-        let output = `${firstName}, You are ${age}, born in ${birthYear}`;
-        console.log(output);
+// function calcAge(birthYear){
+//     const age = 2037 - birthYear;
+//     // console.log(firstName);
+//     function printAge(){
+//         let output = `${firstName}, You are ${age}, born in ${birthYear}`;
+//         console.log(output);
 
-        if(birthYear >= 1981 && birthYear <= 1996){
-            var millenial = true;
-            const firstName = 'Steven';
-            const output = 'NEW OUTPUT!';
-            const str = `Oh, and you're a millenial, ${firstName}`;
-            console.log(str);
-            function add(a, b){
-                return a + b;
-            }
-            console.log(millenial);
-            // add(2,3);
-            console.log(output);
-        }
-    }
-    printAge();
+//         if(birthYear >= 1981 && birthYear <= 1996){
+//             var millenial = true;
+//             const firstName = 'Steven';
+//             const output = 'NEW OUTPUT!';
+//             const str = `Oh, and you're a millenial, ${firstName}`;
+//             console.log(str);
+//             function add(a, b){
+//                 return a + b;
+//             }
+//             console.log(millenial);
+//             // add(2,3);
+//             console.log(output);
+//         }
+//     }
+//     printAge();
 
-    return age;
-}
-const firstName = 'Jonas';
-calcAge(1991);
-// console.log(age);
-// printAge();
+//     return age;
+// }
+// const firstName = 'Jonas';
+// calcAge(1991);
+// // console.log(age);
+// // printAge();
+
+// 18/12/2022
+
+console.log(me);
+console.log(job);
+console.log(year);
+
+var me = 'Jonas';
+let job = 'teacher';
+const year = 1991;
