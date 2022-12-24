e9ad474000fd6cf6df0cd7c559072824b1ac4237
nivya <nivya.mathew@vonnue.com>
2022-12-24 11:32:43 +0530

    feat:section:13

--- Udemy/section-13/script.js
+++ Udemy/section-13/script.js
@@ -137,24 +137,30 @@ const randomInt = (min,max) =>
 Math.floor(Math.random() * (max - min + 1) + min);
 const randomColor = () =>
 `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0,255)})`;
-console.log(randomColor);
+// console.log(randomColor);
 
 document.querySelector('.nav_link').addEventListener('click',function(e){
-    console.log('LINK');
+    // console.log('LINK');
     this .style.backgroundColor = randomColor();
-    console.log('LINK', e.target);
+    console.log('LINK', e.target, e.currentTarget);
+    console.log(e.currentTarget === this);
+
+    // stopPropagation
+    // e.stopPropagation();
 });
 document.querySelector('.nav_link').addEventListener('click',function(e){
     console.log('LINK');
     this .style.backgroundColor = randomColor();
-    console.log('CONTAINER', e.target);
+    console.log('CONTAINER', e.target, e.currentTarget);
 
 });
 document.querySelector('.nav_link').addEventListener('click',function(e){
     console.log('LINK');
     this .style.backgroundColor = randomColor();
-    console.log('NAV', e.target);
+    console.log('NAV', e.target, e.currentTarget);
 
-});
+}, 
+true
+);
 
 
