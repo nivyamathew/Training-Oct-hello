5d3e998a6ec4b6ff870d992fcb7e1c9234c59558
nivya <nivya.mathew@vonnue.com>
2023-01-09 16:34:17 +0530

    feat: practice

---
+++ 50projects/project-0/script.js
@@ -0,0 +1,65 @@
+// EG
+function hello(){
+    return "hello edabit.com"
+}
+
+// sum of two numbers
+function addition(a, b){
+ console.log(a + b)
+}
+addition(2,8)
+
+// minutes to seconds
+function convert(minutes){
+    console.log(minutes * 60)
+}
+convert(5)
+
+// next number
+ function addition(a){
+    console.log(a + 1)
+ }
+ addition(-3)
+
+//  Area of triangle
+ function area (b, h){
+    console.log(1/2 * b * h)
+ }
+ area(7, 4);
+
+//  age to days
+ function age (a){
+    console.log( a *365)
+ }
+ age(20)
+
+//  First element in  an array
+ function getFirstValue(a){
+    console.log(a[1])
+ }
+ getFirstValue([80, 5, 100])
+
+
+//  power calculator
+ function circuitPower(a, b){
+    console.log( a * b)
+ }
+ circuitPower(230, 10)
+
+// hours into seconds
+  function hours(a){
+    console.log(a * 60 *60)
+ }
+ hours(24)
+
+//  edge of a triangle
+ function edge( a, b){
+    console.log((a + b) - 1)
+ }
+ edge(5, 7)
+
+//  REMAINDER FROM TWO NUMBERS
+ function remainder(a ,b){
+    console.log( a % b)
+ }
+ remainder(3, 4)
